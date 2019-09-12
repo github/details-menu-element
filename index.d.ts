62a2bb1f03475c849542d0d84e9783319d4599af
Keith Cirkel <keithamus@users.noreply.github.com>
2019-09-12 10:10:13 +0100

    types(ts): add window declaration

--- index.d.ts
+++ index.d.ts
@@ -2,3 +2,9 @@ export default class DetailsMenuElement extends HTMLElement {
   preload: boolean;
   src: string;
 }
+
+declare global {
+  interface Window {
+    DetailsMenuElement: DetailsMenuElement
+  }
+}
